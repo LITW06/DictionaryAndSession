fe34dae549b54b094c55a4b41cfdcf9ea0fffc03
Avrumi Friedman <info@lakewoodprogramming.com>
2019-03-11 22:29:22 -0400

    Initial commit

---
+++ WebApplication12/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication12.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string text)
+        {
+            HomePageViewModel viewModel = new HomePageViewModel();
+            if (!String.IsNullOrEmpty(text))
+            {
+                viewModel.Text = text;
+                viewModel.CharacterCounts = CountCharacters(text);
+            }
+            return View(viewModel);
+        }
+
+        private Dictionary<char, int> CountCharacters(string text)
+        {
+            Dictionary<char, int> counts = new Dictionary<char, int>();
+            foreach (char c in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+            {
+                counts.Add(c, 0);
+            }
+
+            foreach (char c in text.ToUpper())
+            {
+                if (counts.ContainsKey(c))
+                {
+                    counts[c]++;
+                }
+            }
+
+            return counts;
+        }
+    }
+
+    public class HomePageViewModel
+    {
+        public Dictionary<char, int> CharacterCounts { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ WebApplication12/Controllers/SessionDemoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication12.Controllers
+{
+    public class Counter
+    {
+        public int Number { get; set; }
+    }
+
+    public class SessionDemoController : Controller
+    {
+        public ActionResult Index()
+        {
+            Counter c = null;
+            if (Session["counter"] != null)
+            {
+                c = (Counter) Session["counter"];
+            }
+            else
+            {
+                c = new Counter();
+                Session["counter"] = c;
+            }
+
+            c.Number++;
+            return View(c);
+        }
+
+        public ActionResult ViewForm()
+        {
+            NameClass nc = new NameClass
+            {
+                Name = (string) Session["name"]
+            };
+            return View(nc);
+        }
+
+        public ActionResult FormPost(string name)
+        {
+            Session["name"] = name;
+            
+            return Redirect("/sessiondemo/viewform");
+        }
+    }
+
+    public class NameClass
+    {
+        public string Name { get; set; }
+    }
+}
